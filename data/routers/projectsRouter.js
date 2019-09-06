25cc4901442a6db497c7d83a79949eab5f5e31d8
Pat Palmerston <patpalmerston@gmail.com>
2019-09-05 22:20:15 -0700

    projects update working

--- data/routers/projectsRouter.js
+++ data/routers/projectsRouter.js
@@ -58,5 +58,17 @@ router.delete('/:id', (req, res) => {
 });
 
 //   update,
+router.put('/:id', (req, res) => {
+	const { id } = req.params;
+	const { name, description } = req.body;
+	ProjectDb.update(id, { name, description })
+		.then(project => {
+			res.status(200).json({ project });
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ error: 'unable to edit project' });
+		});
+});
 
 module.exports = router;
