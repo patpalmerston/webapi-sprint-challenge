53c9c3fb3b7f61faeaab19e1d8d0a286a79f4cdb
Pat Palmerston <patpalmerston@gmail.com>
2019-09-05 22:04:07 -0700

    posting projects

--- data/routers/projectsRouter.js
+++ data/routers/projectsRouter.js
@@ -14,23 +14,34 @@ router.get('/', (req, res) => {
 			console.log(err);
 			res.status().json({ error: 'unable to get project info' });
 		});
-  });
+});
 
 //   getProjectActions,
 router.get('/:id/actions', (req, res) => {
-  const {id} = req.params
-  ProjectDb.getProjectActions(id)
-    .then(actions => {
-      res.status(200).json(actions)
-    })
-    .catch(err => {
-      console.log(err)
-      res.status(500).json({error: 'unable to retrieve action info'})
-    })
-})
+	const { id } = req.params;
+	ProjectDb.getProjectActions(id)
+		.then(actions => {
+			res.status(200).json(actions);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ error: 'unable to retrieve action info' });
+		});
+});
 
 //   insert,
-//   update,
+router.post('/', (req, res) => {
+	const { name, description } = req.body;
+	ProjectDb.insert({ name, description })
+		.then(project => {
+			res.status(200).json(project);
+		})
+		.catch(err => {
+			console.log(err);
+			res.status(500).json({ error: 'unable to make new project' });
+		});
+});
 //   remove,
+//   update,
 
 module.exports = router;
