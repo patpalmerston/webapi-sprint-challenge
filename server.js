6e0713271540007e53f561ceeead5ba73179073d
Pat Palmerston <patpalmerston@gmail.com>
2019-09-05 21:26:52 -0700

    global middleware file implemented

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const configureMiddleware = require('./data/middleware/middleware')
+
 
 const server = express();
+configureMiddleware(server)
 
 server.use('/', (req, res) => {
 	res.send('<h2>Lets Party</h2>');
